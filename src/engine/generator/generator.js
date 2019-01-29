e758a368efa7025baeab9e1d6855ff2c7e81cd4d
ppizarror <pablo.pizarro@ing.uchile.cl>
2019-01-29 17:49:34 +0000

    Added basic generator instance

---
+++ src/engine/generator/generator.js
@@ -0,0 +1,60 @@
+/**
+ GENERATOR
+ Global class.
+
+ @author Pablo Pizarro R. @ppizarror.com
+ @license MIT
+ */
+"use strict";
+
+/**
+ * Generator class.
+ *
+ * @class
+ * @abstract
+ */
+function Generator() {
+
+    /**
+     * Object pointer.
+     * @type {Generator}
+     */
+    let self = this;
+
+    /**
+     * ID of the generator.
+     * @type {string}
+     * @protected
+     */
+    this._id = generateID();
+
+    /**
+     * Generator volume.
+     * @type {Volume}
+     * @protected
+     */
+    this._volume = new Volume();
+
+    /**
+     * Generate element, space volume goes from (xi,yi,zi) to (xf,yf,zf).
+     *
+     * @function
+     * @param {number} xi - Initial X coordinate
+     * @param {number} yi - Initial Y coordinate
+     * @param {number} zi - Initial Z coordinate
+     * @param {number} xf - End X coordinate
+     * @param {number} yf - End Y coordinate
+     * @param {number} zf - End Z coordinate
+     */
+    this.generate = function (xi, yi, zi, xf, yf, zf) {
+    };
+
+    /**
+     * Draw the generated figure.
+     *
+     * @function
+     */
+    this.draw = function () {
+    };
+
+}
