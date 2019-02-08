58452ebd50a3b39d2988388598192d2f4d0f9db4
ppizarror <pablo.pizarro@ing.uchile.cl>
2019-02-08 15:47:40 +0000

    Added empty generator

---
+++ src/engine/generator/empty.js
@@ -0,0 +1,42 @@
+/**
+ EMPTY GENERATOR
+ Empty generator.
+
+ @author Pablo Pizarro R. @ppizarror.com
+ @license MIT
+ */
+"use strict";
+
+/**
+ * This generator does nothing.
+ *
+ * @class
+ * @extends {Generator}
+ */
+function EmptyGenerator() {
+
+    /**
+     * Inherit class.
+     */
+    Generator.call(this);
+
+    // noinspection JSUnusedLocalSymbols, JSUnusedGlobalSymbols
+    /**
+     * Generate element, space volume goes from (xi,yi,zi) to (xf,yf,zf).
+     *
+     * @function
+     * @param {number} xi - Initial X coordinate
+     * @param {number} yi - Initial Y coordinate
+     * @param {number} zi - Initial Z coordinate
+     * @param {number} xf - End X coordinate
+     * @param {number} yf - End Y coordinate
+     * @param {number} zf - End Z coordinate
+     */
+    this._generate = function (xi, yi, zi, xf, yf, zf) {
+
+        // Place camera
+        this._set_camera_position(0, 0, 1);
+
+    };
+
+}
